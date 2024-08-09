5e80239671894fbd91514ba234fccdc350f64732
sylla <s67986601@gmail.com>
2024-08-09 01:01:21 +0000

    hotfix(vite): i add more limit ont vite chunck

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { chunkSizeWarningLimit: 2000, }
+  build: { chunkSizeWarningLimit: 100000000000, }
 })
